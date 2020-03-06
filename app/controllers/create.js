c8d8fb2f2377db0d6e1a4a9c04f46e44b873eaf5
Omar Estrella <omarestrella@bitcreative.net>
2020-03-06 11:15:46 -0500

    Something from the merge

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -15,14 +15,10 @@ export default class CreateController extends Controller {
       "height": this.size,
       "width": this.size
     });
-<<<<<<< HEAD
-    this.logger.log("Created match", match);
-=======
     this.transitionToRoute('match', match);
   }
 
   @action setSize(event) {
     this.size = event.target.value;
->>>>>>> 13eec8642587ee2989311a0b61b13895a04c24c9
   }
 }
